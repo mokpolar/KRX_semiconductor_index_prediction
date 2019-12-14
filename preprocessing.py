98ded0f227a56ade40b2c5c9ce6f66425974f369
Jung Ju Young <beholder16@gmail.com>
2019-12-14 20:57:54 +0900

    baseline1

--- preprocessing.py
+++ preprocessing.py
@@ -41,6 +41,15 @@ krx_index = pd.read_csv('CSV/KRXsemiconductor_wd.csv')
 hynix = pd.read_csv('CSV/SKhynix_wd.csv')
 tsmc = pd.read_csv('CSV/TSMC.csv')
 phil = pd.read_csv('CSV/Philadelphia.csv')
+
+#%%
+
+fb = pd.read_csv('CSV/Facebook.csv')
+google = pd.read_csv('CSV/Google.csv')
+intel = pd.read_csv('CSV/Intel.csv')
+micron = pd.read_csv('CSV/Micron.csv')
+nv = pd.read_csv('CSV/Nvidia.csv')
+
 # %%
 krx_index.shape
 
@@ -146,5 +155,83 @@ df.head()
 # %%
 
 df.to_csv('df_test.csv')
+
+# %%
+fb.head()
+
+# %%
+google.head()
+
+# %%
+intel.head()
+
+# %%
+micron.head()
+#%%
+
+nv.head()
+
+# %%
+fb['date'] = pd.to_datetime(fb['DATE'])
+google['date'] = pd.to_datetime(google['DATE'])
+intel['date'] = pd.to_datetime(intel['DATE'])
+micron['date'] = pd.to_datetime(micron['DATE'])
+nv['date'] = pd.to_datetime(nv['DATE'])
+
+
+# %%
+df = pd.read_csv('df_test.csv')
+
+# %%
+df = pd.merge(df, nv, on = 'date', how = 'left')
+
+
+# %%
+
+# %%
+df.head()
+
+# %%
+df.columns = ['0', 'date', 'krx_close', 'krx_volume', 'hy_close',
+       'hy_volume', 'ts_close', 'ts_volume', 'fbDATE_x', 'fbOPEN_x', 'fbHIGH_x',
+       'fbLOW_x', 'fb_close', 'fb_volume', 'fbCHANGE_x', 'gooDATE_y', 'gooOPEN_y',
+       'gooHIGH_y', 'gooLOW_y', 'google_close', 'google_volume', 'gooCHANGE_y', 'inDATE_x',
+       'inOPEN_x', 'inHIGH_x', 'inLOW_x', 'intel_close', 'intel_volume', 'inCHANGE_x',
+       'miDATE_y', 'miOPEN_y', 'miHIGH_y', 'miLOW_y', 'micron_close', 'micron_volume',
+       'miCHANGE_y', 'vnDATE', 'nvOPEN', 'nvHIGH', 'nvLOW', 'nv_close', 'nv_volume', 'nvCHANGE']
+
+# %%
+df2 = df[['date', 'krx_close', 'krx_volume', 'hy_close', 'hy_volume', 'ts_close', 'ts_volume', 'fb_close', 'fb_volume', 'google_close', 'google_volume', 'intel_close', 'intel_volume', 'micron_close', 'micron_volume', 'nv_close', 'nv_volume']]
+
+# %%
+df2.head()
+
+# %%
+# facebook은 생긴지 얼마 안되서 그런지 데이터 너무 없다. 무려 1500개가 null값임
+
+df2.to_csv('df_test2.csv')
+
+# %%
+df2.info()
+
+# %%
+sns.distplot(df2['krx_close'], hist = False).set_title("KRX index distribution")
+
+
+# %%
+sns.distplot(df2['hy_close'], hist = False).set_title("Hynix distribution")
+
+# %%
+df2['hy_close'] = df2['hy_close'].map(lambda x: re.sub("\D", "", x))
+df2['hy_volume'] = df2['hy_volume'].map(lambda x: re.sub("\D", "", x))
+# %%
+df2['hy_close'] = pd.to_numeric(df2['hy_close'])
+df2['hy_volume'] = pd.to_numeric(df2['hy_volume'])
+
+# %%
+df2.info()
+
+# %%
+df2.to_csv('df_test2.csv')
 
 # %%
